0b048f6b16a2353bcec07cb2a5d5eb9c861edbff
a875221443 <875221443@qq.com>
2016-11-03 18:55:11 +0800

    Initial commit

---
+++ app/src/main/java/bawei/okhttp/RequestParameter.java
@@ -0,0 +1,15 @@
+package bawei.okhttp;
+
+/**
+ * Created by yekh on 2016/11/3.
+ */
+
+public class RequestParameter {
+    /**
+     * page : 1
+     * rows : 20
+     */
+
+    public String page;
+    public String rows;
+}
